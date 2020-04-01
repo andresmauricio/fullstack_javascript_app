36e216b93350227302378eb068c1794b7f3acd5e
andresmauricio <andresacelas@outlook.com>
2020-04-01 18:50:19 -0500

    create rest api

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,9 +1,23 @@
 const { Router } = require('express');
 const router = Router();
 
-router.get('/', (req, res) => { 
-    res.json({text: 'hello baby'})
-    
-})
+const Book = require('../models/Book');
+
+router.get('/', async (req, res) => { 
+    const books = await Book.find();    
+    res.json(books)    
+});
+
+router.post('/', async (req, res) => {
+    const {title, author, isbn} = req.body;
+    const newBook = new Book({title, author, isbn});
+    await newBook.save();
+    res.json(newBook);
+});
+
+router.delete('/:id', async (req, res) => {
+    await Book.findByIdAndDelete(req.param.id);
+    res.json({result: true});
+});
 
 module.exports = router;
