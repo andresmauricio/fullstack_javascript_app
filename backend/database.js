53d52397ec04428e7750a7704dbd6ad2bf444e0e
andresmauricio <andresacelas@outlook.com>
2020-03-31 21:52:18 -0500

    enviroments whit dotenv TODO

--- backend/database.js
+++ backend/database.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/jsvascriptdb', {
-    useNewUrlParser: true 
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
 })
     .then(db => console.log('Database is connected'))
     .catch(error => console.log('This is error:', error))
